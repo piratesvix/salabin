a52087643d1124a25db339f075572c9b2b43755b
ahsouza <annibalhsouza@gmail.com>
2023-01-26 14:39:42 -0300

    apply connections in hand directions

--- tracking.py
+++ tracking.py
@@ -6,12 +6,17 @@ target = cv2.VideoCapture(0)
 mov = mp.solutions.hands
 salabin = mov.Hands()
 
+targetDraw = mp.solutions.drawing_utils
+
 
 while True:
     success, frame = target.read()
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     res = salabin.process(frameRGB)
-    print(res)
+    
+    if res.multi_hand_landmarks:
+      for hand in res.multi_hand_landmarks:
+        targetDraw.draw_landmarks(frame, hand, mov.HAND_CONNECTIONS)
 
     cv2.imshow("Image", frame)
     cv2.waitKey(1)
